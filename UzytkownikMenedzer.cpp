9f36e8c6831a12c6bfc86bc038e23c8ae2a2e9ad
Bartlomiej Adamczewski <bartlomiej.adamczewski@gmail.com>
2022-11-17 20:33:18 +0100

    Upgrade log in function - ask for another login when last one was wrong

--- UzytkownikMenedzer.cpp
+++ UzytkownikMenedzer.cpp
@@ -20,32 +20,6 @@ int UzytkownikMenedzer::logowanieUzytkownika()
     cout << endl << "Podaj login: ";
     login = MetodyPomocnicze::wczytajLinie();
 
-    /*vector <Uzytkownik>::iterator itr = uzytkownicy.begin();
-    while (itr != uzytkownicy.end())
-    {
-        if (itr -> login == login)
-        {
-            for (int iloscProb = 3; iloscProb > 0; iloscProb--)
-            {
-                cout << "Podaj haslo. Pozostalo prob: " << iloscProb << ": ";
-                haslo = MetodyPomocnicze::wczytajLinie();
-
-                if (itr -> haslo == haslo)
-                {
-                    cout << endl << "Zalogowales sie." << endl << endl;
-                    system("pause");
-                    return itr -> id;
-                }
-            }
-            cout << "Wprowadzono 3 razy bledne haslo." << endl;
-            system("pause");
-            return 0;
-        }
-        itr++;
-    }
-    cout << "Nie ma uzytkownika z takim loginem" << endl << endl;
-    system("pause");
-    return 0;*/
     for(int i = 0; i < uzytkownicy.size(); i++)
     {
         if (uzytkownicy[i].pobierzLogin() == login)
@@ -65,6 +39,12 @@ int UzytkownikMenedzer::logowanieUzytkownika()
             cout << "Wprowadzono 3 razy bledne haslo." << endl;
             system("pause");
             return 0;
+        }
+        else
+        {
+            cout << endl << "Bledny login, sprobuj ponownie. ";
+            cout << endl << "Podaj login: ";
+            login = MetodyPomocnicze::wczytajLinie();
         }
 
     }
